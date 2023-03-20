73f4d0691d5d07bde0c05d3a39cd947bce7d8d71
Arna Zugor <zugor.arna@gmail.com>
2023-03-20 15:24:00 +0100

    Homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,112 @@
 public class Main {
     public static void main(String[] args) {
 
+    //    int someRandomNumber = 243;
+    //    char someRandomCharacter = 'someRandomNumber';
+
+    //    String helloText = "Hi";
+    //    double someRandomNumber = helloText;
 
 
-        System.out.println("Hello world!");
+        String arnasFullName = "Arna Zugor";
+        char firstLetterOfArnasName = 'A';
+        int yearOfBirth = 1985;
+        long randomLongNumber = 100000000000L;
+        int randomShortNumber = 42001;
+        float randomDecimalNumber = 42.001f;
+
+
+        System.out.println(arnasFullName);
+        System.out.println(firstLetterOfArnasName);
+        System.out.println(yearOfBirth);
+        System.out.println(randomLongNumber);
+        System.out.println(randomShortNumber);
+        System.out.println(randomDecimalNumber);
+
+
+        char firstLetterOfName = '\u0041';
+        char secondLetterOfName = '\u0072';
+        char thirdLetterOfName = '\u006E';
+        char fourthLetterOfName = '\u0061';
+
+
+        System.out.print(firstLetterOfName);
+        System.out.print(secondLetterOfName);
+        System.out.print(thirdLetterOfName);
+        System.out.println(fourthLetterOfName);
+
+
+        boolean answerToTheUltimateQuestionOfLifeTheUniverseAndEverythingIsTheNumber42 = true;
+
+        System.out.println(answerToTheUltimateQuestionOfLifeTheUniverseAndEverythingIsTheNumber42);
+
+
+        int workingNumber = 100;
+        int doubledValue = workingNumber*2;
+        int numberOfDoubles = 1;
+
+        System.out.println("This is number " + workingNumber + " and it is about to double: " + doubledValue);
+        System.out.println("The original number has been doubled: " + numberOfDoubles + " time");
+
+
+        workingNumber = 200;
+        doubledValue = workingNumber*2;
+        numberOfDoubles = 2;
+
+        System.out.println("This is number " + workingNumber + " and it is about to double: " + doubledValue);
+        System.out.println("The original number has been doubled: " + numberOfDoubles + " times");
+
+
+        workingNumber = 300;
+        doubledValue = workingNumber*2;
+        numberOfDoubles = 3;
+
+        System.out.println("This is number " + workingNumber + " and it is about to double: " + doubledValue);
+        System.out.println("The original number has been doubled: " + numberOfDoubles + " times");
+
+
+        workingNumber = 400;
+        doubledValue = workingNumber*2;
+        numberOfDoubles = 4;
+
+        System.out.println("This is number " + workingNumber + " and it is about to double: " + doubledValue);
+        System.out.println("The original number has been doubled: " + numberOfDoubles + " times");
+
+
+        workingNumber = 500;
+        doubledValue = workingNumber*2;
+        numberOfDoubles = 5;
+
+        System.out.println("This is number " + workingNumber + " and it is about to double: " + doubledValue);
+        System.out.println("The original number has been doubled: " + numberOfDoubles + " times");
+
+
+        workingNumber = 600;
+        doubledValue = workingNumber*2;
+        numberOfDoubles = 6;
+
+        System.out.println("This is number " + workingNumber + " and it is about to double: " + doubledValue);
+        System.out.println("The original number has been doubled: " + numberOfDoubles + " times");
+
+
+        workingNumber = 700;
+        doubledValue = workingNumber*2;
+        numberOfDoubles = 7;
+
+        System.out.println("This is number " + workingNumber + " and it is about to double: " + doubledValue);
+        System.out.println("The original number has been doubled: " + numberOfDoubles + " times");
+
+
+        workingNumber = 800;
+        doubledValue = workingNumber*2;
+        numberOfDoubles = 8;
+
+        System.out.println("This is number " + workingNumber + " and it is about to double: " + doubledValue);
+        System.out.println("The original number has been doubled: " + numberOfDoubles + " times");
+
+
+
+
+
     }
 }
